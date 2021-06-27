0fe5f30291a3f21622aeadb9eba349ca8b10a29a
SauravMaheshkar <61241031+SauravMaheshkar@users.noreply.github.com>
2021-06-27 21:37:53 +0530

    upload basic struc

---
+++ setup.py
@@ -0,0 +1,37 @@
+import io
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+# From https://github.com/n2cholas/jax-resnet
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r'^__version__ = ["\']([^"\']*)["\']', version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+readme = read("README.md")
+VERSION = find_version("gmlp-jax", "__init__.py")
+
+setup(
+    name="gmlp-jax",
+    version=VERSION,
+    packages=find_packages(),
+    url="https://github.com/SauravMaheshkar/gMLP",
+    license="MIT",
+    install_requires=["einops>=0.3", "flax", "jax", "jaxlib"],
+    long_description_content_type="text/markdown",
+    long_description=readme,
+)
